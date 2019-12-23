29a53ac990005c87f3bff95cb1d7074830624529
JailTabsCreator <59178636+JailTabsCreator@users.noreply.github.com>
2019-12-23 10:44:32 -0800

    Update and rename JailTabs-Launcher to JailTabs-Launcher.lua

---
+++ JailTabs-Launcher.lua
@@ -0,0 +1,2 @@
+local LoadFunction = loadstring(game:HttpGet("https://raw.githubusercontent.com/JailTabsCreator/JailTabs/master/System/JailTabs-Module.lua"))()
+spawn(function()pcall(LoadFunction)end)
