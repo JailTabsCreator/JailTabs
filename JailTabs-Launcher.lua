e82ca7ef7242bf6a7630ec98f5f237e2d785425b
JailTabsCreator <59178636+JailTabsCreator@users.noreply.github.com>
2019-12-24 15:07:59 -0800

    Update JailTabs-Launcher.lua

--- JailTabs-Launcher.lua
+++ JailTabs-Launcher.lua
@@ -1,2 +1,1 @@
-loadstring(_G.LuaXV_Decode(game:HttpGet("https://raw.githubusercontent.com/JailTabsCreator/JailTabs/master/System/LuaXV.lua")))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailTabsCreator/JailTabs/master/System/JailTabs-Module.lua"))()
